6871c92f01798f9632af6af055b41984e3a73a27
Roberto Romero <roberto@ulama.co>
2022-06-09 21:40:49 -0500

    repaso pokemon

--- repaso-roberto/pokedex/js/app.js
+++ repaso-roberto/pokedex/js/app.js
@@ -11,7 +11,39 @@ function render(){
     //recorremos el array pokemones
     //for / forEach / for in
     for(var i in pokemones){
-        console.log(pokemones[i].name);
+        console.log(pokemones[i])
+        let card = `<div class="col-3">
+            <div class="card mt-4">
+                <img src="${pokemones[i].ThumbnailImage}" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">${pokemones[i].name}</h5>
+                    <p class="card-text">#${pokemones[i].number}</p>`;
+            /*Aqui puedo manipular mis tipos*/
+            /*Esta no la voy a usar
+            card+=`<span class="badge text-bg-primary">${pokemones[i].type[0]}</span>
+                    <span class="badge text-bg-danger">${pokemones[i].type[1]}</span>`;
+            */
+            console.log(pokemones[i].type)
+
+            let tipos = pokemones[i].type;
+
+            /*for(var j=0; j<pokemones[i].type.length; j++){
+                card+=`<span class="badge text-bg-primary">${pokemones[i].type[j]}</span>`
+            }*/
+
+            for(var j=0; j<tipos.length; j++){
+                card+=`<span class="badge text-bg-primary">${tipos[j]}</span>`;
+            }
+
+            /*Ya los manipule*/
+
+        card+=`</div>
+            </div>
+        </div>`;
+        document.querySelector("#resultado").innerHTML += card;
+
+        if(i==10) break;
+        //console.log(pokemones[i].name);
     }
     //termina el for
 
