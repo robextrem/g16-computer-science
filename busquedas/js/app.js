808c4885a24fcdb008897bc407e3e71a543cfc5d
Roberto Romero <roberto@ulama.co>
2022-06-16 20:07:27 -0500

    busqueda

--- busquedas/js/app.js
+++ busquedas/js/app.js
@@ -1,7 +1,10 @@
 let pokemones = [];
+let favoritos = [];
 const formSearch = document.querySelector("#search-pokemon");
 const result = document.querySelector("#resultado");
 const input = document.querySelector("#pokemon");
+const tipos = document.querySelector("#tipos");
+
 const buttonWater = document.querySelector("#button-water");
 const buttonFire = document.querySelector("#button-fire");
 const buttonElectric = document.querySelector("#button-electric");
@@ -9,7 +12,7 @@ const buttonAsc = document.querySelector("#button-asc");
 const buttonDesc = document.querySelector("#button-desc");
 const buttonAscNum = document.querySelector("#button-asc-num");
 const buttonDescNum = document.querySelector("#button-desc-num");
-
+const buttonFavoritos = document.querySelector("#button-favoritos");
 
 /**Esta es una busqueda lineal
  * 
@@ -32,6 +35,7 @@ pokemones.filter(function(pokemon){
 
 formSearch.addEventListener("submit",search);
 input.addEventListener("keyup",search)
+
 buttonWater.addEventListener("click",searchWater);
 buttonFire.addEventListener("click", searchFire);
 buttonElectric.addEventListener("click", searchElectric)
@@ -39,6 +43,8 @@ buttonAsc.addEventListener("click", orderAsc)
 buttonDesc.addEventListener("click",orderDesc)
 buttonAscNum.addEventListener("click",reiniciar)
 buttonDescNum.addEventListener("click",orderNumDesc)
+tipos.addEventListener("change",searchByType)
+buttonFavoritos.addEventListener("click",listFavs)
 
 fetch('js/pokemones.json')
 .then(response => response.json())
@@ -47,6 +53,9 @@ fetch('js/pokemones.json')
     render(pokemones);
 });
 
+function listFavs(){
+    render(favoritos);
+}
 
 function reiniciar(){
     pokemones.sort(function(pokemon1,pokemon2){
@@ -103,6 +112,15 @@ function search(e){
     render(pokemonesFiltrados);
 }
 
+
+function searchByType(e){
+    let t = e.target.value;
+    let pokemonesFiltrados = pokemones.filter(function(pokemon){
+        return pokemon.type.includes(t);
+    });
+    render(pokemonesFiltrados);
+}
+
 function searchWater(){
     let pokemonesFiltrados = pokemones.filter(function(pokemon){
         return pokemon.type.includes("water");
@@ -131,8 +149,38 @@ function render(pokemones){
         let column = document.createElement("div");
         column.classList.add("col-3")
 
+        let button = document.createElement("button");
+        button.innerHTML="Agregar a favorito";
+        button.classList.add("btn","mt-3", "btn-warning");
+        button.setAttribute("data-number",pokemones[i].number);
+        button.setAttribute("data-nombre",pokemones[i].name);
+        button.setAttribute("data-index",i);
+        //button.dataset.id=pokemones[i].number;
+        button.addEventListener("click",function(evt){
+            let miboton = evt.target;
+            let name = miboton.dataset.nombre;
+            let num = miboton.dataset.number;
+            let index = miboton.dataset.index;
+
+                let aux_favoritos = favoritos.filter(function(favorito){
+                    return favorito.number === num;
+                });
+
+                console.log(aux_favoritos);
+
+                if(aux_favoritos.length<=0){
+                    //Si ya existe en el arreglo
+                    favoritos.push(pokemones[index]);
+                    alert(`El pokemon ${name} se ha agregado a favoritos`);
+                }else{
+                    alert(`El pokemon ${name} ya estaba en la lista`);
+                }
+
+        });
+
+
         let card = `<div class="card mt-4">
-                <img src="${pokemones[i].ThumbnailImage}" loading="lazy" class="card-img-top">
+                <img data-number="${pokemones[i].number}" src="${pokemones[i].ThumbnailImage}" loading="lazy" class="card-img-top">
                 <div class="card-body">
                     <h5 class="card-title">${pokemones[i].name}</h5>
                     <p class="card-text">#${pokemones[i].number}</p>`;
@@ -143,9 +191,14 @@ function render(pokemones){
                 card+=`<span class="badge ms-1 text-bg-primary">${tipos[j]}</span>`;
             }
 
-        card+=`</div>
+        card+=`
+            </div>
             </div>`;
+
+
         column.innerHTML=card;
+        column.appendChild(button);
+
         result.append(column);
     }
 }
